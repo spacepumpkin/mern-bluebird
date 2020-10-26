1a14245dd4cf73327c0b6206dc9890f86777c1f0
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2020-10-26 05:00:26 -0700

    Push to heroku

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 // Express Framework
 const express = require('express');
 // Mongoose ODM
